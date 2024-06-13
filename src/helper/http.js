d7d31183b66ba03fdd6df4bc91103638422216f3
thornbug90 <crazydev0409@gmail.com>
2024-06-13 05:41:08 -0400

    fix: replace http to https

--- src/helper/http.js
+++ src/helper/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const http = axios.create({
   // baseURL: "https://hotel-booking-backend-splu.onrender.com/api",
-  baseURL: "http://hotelbooking.render.com.marketmajesty.net/api",
+  baseURL: "https://hotelbooking.render.com.marketmajesty.net/api",
   // baseURL: "http://localhost:4001/api",
   headers: {
     "Content-Type": "application/json",
@@ -10,5 +10,5 @@ export const http = axios.create({
 });
 
 export const uploadPath =
-  "http://hotelbooking.render.com.marketmajesty.net/uploads/";
+  "https://hotelbooking.render.com.marketmajesty.net/uploads/";
 // export const uploadPath = "http://localhost:4001/uploads/";
